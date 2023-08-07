827bc04ac74f2e2f2410e4e65d47279b2f173055
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-07 09:21:11 +0300

    eslint

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,9 @@
 import './style/common.css';
 import './style/App.css';
 import './style/responsive.css';
-import MainBtn from './components/MainBtn';
 import Card1 from './components/Card1';
-import Card2 from './components/Card2';
 import Header from './components/layout/Header';
 // export import Card2
-
-function Card3() {
-  const cityName = 'San Francisco !!!';
-  return (
-    <article className='singleCard'>
-      <img src='/img/sanfran.jpg' alt='new york' className='cardImg' />
-      <div className='cardInfo'>
-        <h3 className='cardTitle'>{cityName}</h3>
-        <p className='cardDate'>Fri 17 Nov 2017</p>
-        <p className='text'>
-          Praesent tincidunt sed tellus ut rutrum sed vitae justo.
-        </p>
-        <MainBtn />
-      </div>
-    </article>
-  );
-}
 
 function App() {
   // const now = new Date();
@@ -32,9 +13,17 @@ function App() {
     <div className='App container'>
       <Header />
       <div className='cardsList grid3'>
-        <Card1 city='New York !' heroImage='/img/newyork.jpg' />
-        <Card1 city='Paris !' heroImage='/img/paris.jpg' />
-        <Card1 city='San Francisco !' heroImage='/img/sanfran.jpg' />
+        <Card1
+          city='New York !'
+          heroImage='/img/newyork.jpg'
+          date='2023-07-08'
+        />
+        <Card1 city='Paris !' heroImage='/img/paris.jpg' date='2024-07-08' />
+        <Card1
+          city='San Francisco !'
+          heroImage='/img/sanfran.jpg'
+          date='2019-07-08'
+        />
       </div>
     </div>
   );
--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -6,10 +6,10 @@ function Card1(props) {
   return (
     <div>
       <article className='singleCard'>
-        <img src='/img/newyork.jpg' alt='new york' className='cardImg' />
+        <img src={props.heroImage} alt='new york' className='cardImg' />
         <div className='cardInfo'>
           <h3 className='cardTitle'>{cityName}</h3>
-          <p className='cardDate'>Fri 27 Nov 2016</p>
+          <p className='cardDate'>{props.date}</p>
           <p className='text'>
             Praesent tincidunt sed tellus ut rutrum sed vitae justo.
           </p>
