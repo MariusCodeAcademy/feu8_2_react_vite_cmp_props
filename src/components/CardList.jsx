cbe8c0b1e68dcc05f9c62a9518e1ffe446c28db0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-07 11:39:54 +0300

    map

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,25 +1,28 @@
 import Card1 from './Card1';
 
-const card1Data = {
-  city: 'New York !!',
-  heroImage: '/img/newyork.jpg',
-  date: '2023-07-08',
-};
-
 const cardsArr = [
   {
+    id: '1',
     city: 'New York !!',
     heroImage: '/img/newyork.jpg',
     date: '2023-07-08',
   },
   {
+    id: '2',
     city: 'Paris !!',
     heroImage: '/img/paris.jpg',
     date: '2023-07-08',
   },
   {
+    id: '3',
     city: 'San Francisco !!',
     heroImage: '/img/sanfran.jpg',
+    date: '2023-07-08',
+  },
+  {
+    id: '4',
+    city: 'New York !!',
+    heroImage: '/img/newyork.jpg',
     date: '2023-07-08',
   },
 ];
@@ -27,21 +30,14 @@ const cardsArr = [
 function CardList() {
   return (
     <div className='cardsList grid3'>
-      <Card1
-        city={cardsArr[0].city}
-        heroImage={cardsArr[0].heroImage}
-        date={cardsArr[0].date}
-      />
-      <Card1
-        city={cardsArr[1].city}
-        heroImage={cardsArr[1].heroImage}
-        date={cardsArr[1].date}
-      />
-      <Card1
-        city={cardsArr[2].city}
-        heroImage={cardsArr[2].heroImage}
-        date={cardsArr[2].date}
-      />
+      {cardsArr.map((townObj) => (
+        <Card1
+          key={townObj.id}
+          city={townObj.city}
+          heroImage={townObj.heroImage}
+          date={townObj.date}
+        />
+      ))}
     </div>
   );
 }
